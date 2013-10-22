c80885f939c6525a661f1bb2a1d88d41f9aa6463
Administrator <Administrator@w2k3-am71.pslab.com>
2013-10-22 08:52:48 -0400

    Added new testing framework

---
+++ keyservapp/src/test/java/com/lawdocslox/app/ITTest.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.lawdocslox.app;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+//import org.springframework.test.web.servlet.samples.context.WebContextLoader;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+/**
+ *
+ * @author wgoulet
+ */
+public class ITTest {
+    
+}
