9aae5fe60bf36f6d54f8e2d97641b8f4617d391e
wgoulet <>
2013-10-23 00:08:12 -0400

    Updating new spring unit tests

--- keyservapp/src/test/java/com/lawdocslox/app/ITTest.java
+++ keyservapp/src/test/java/com/lawdocslox/app/ITTest.java
@@ -5,17 +5,52 @@
  */
 
 package com.lawdocslox.app;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.web.servlet.MockMvc;
+//import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-//import org.springframework.test.web.servlet.samples.context.WebContextLoader;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.ContextHierarchy;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.resource.DefaultServletHttpRequestHandler;
+
 /**
+ * Tests dependent on access to resources under the web application root directory.
  *
- * @author wgoulet
+ * @author Rossen Stoyanchev
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration("src/test/resources/META-INF/web-resources")
+@ContextHierarchy({
+        @ContextConfiguration("root-context.xml"),
+        @ContextConfiguration("servlet-context.xml")
+})
 public class ITTest {
-    
+
+        @Autowired
+        private WebApplicationContext wac;
+
+        private MockMvc mockMvc;
+
+        @Before
+        public void setup() {
+                this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).alwaysExpect(status().isOk()).build();
+        }
+        
+        @Test
+        public void testgetkey() throws Exception {
+                this.mockMvc.perform(get("/keyserv/key/MjE0NTc2NDM=/MTIzNDU2Nw==")).andReturn();
+                        //.andExpect(forwardedUrl("/WEB-INF/layouts/standardLayout.jsp"));
+        }
 }
