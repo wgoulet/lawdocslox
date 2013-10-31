62b6006ce607d3eb26554241303de66f6aac3d9f
wgoulet@gmail.com <>
2013-10-30 23:29:08 -0500

    Added support for caching and restoring seeding material for the KeyObjFactory to disk. NOTE: Looks like we have a bug; when I change contents of seed and iv on disk, generated keys are the same. Generated keys only change if I use a different secret value.

--- keyservapp/src/main/java/com/lawdocslox/app/JsonService.java
+++ keyservapp/src/main/java/com/lawdocslox/app/JsonService.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.context.annotation.CommonAnnotationBeanPostProcessor;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.core.env.*;
 import org.apache.log4j.ConsoleAppender;
 import org.apache.log4j.Logger;
 import org.apache.log4j.*;
@@ -38,30 +42,90 @@ public class JsonService {
     data.put("github", new Person("rocks", "man!", 3));
             
   }
-  
+
+  @Value ("#{myProps['keyfile']}")
+  private String keyfile;
+
+  @Value("#{myProps['seedsize']}")
+  private int seedsize;
+
+  @Value("#{myProps['initsecret']}")
+  private String initsecret;
+
+  private String seed;
+  private String secret;
+  private String iv;
+
   @PostConstruct
   public void init()
   {
-	  // Look for secret and seed values to initialize KeyObjFactory
-	  // with. If they don't exist, create new ones and store in 
-	  // properties file
-	  Properties props = new Properties();
-	 // String path = getServletContext();
+	// Look for secret and seed values to initialize KeyObjFactory
+	// with. If they don't exist, create new ones and store in 
+	// properties file
+	Properties props = new Properties();
+	File keys = new File(keyfile);
+	if(!keys.exists())
+	{
+		try
+		{
+			keys.createNewFile();
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+	  try
+	  {
+	  	FileInputStream fstream = new FileInputStream(keyfile);
+		
+		props.load(fstream);
+		if((props.containsKey("seed") 
+		&&(props.containsKey("secret")  
+		&&(props.containsKey("iv")))))
+		{
+			seed = props.getProperty("seed");
+			secret = props.getProperty("secret");
+			iv = props.getProperty("iv");
+			KeyObjFactory.setRandomSeed(Base64.decodeBase64(seed));
+			KeyObjFactory.setSecret(Base64.decodeBase64(secret));
+			KeyObjFactory.setIV(Base64.decodeBase64(iv));
+		}
+		else
+		{
+			byte[] seedbytes = new byte[seedsize];
+			byte[] ivbytes = new byte[seedsize];
+			seedbytes = KeyObjFactory.genRandVal(seedsize);
+			props.setProperty("seed",Base64.encodeBase64(seedbytes).toString());
+			props.setProperty("secret", initsecret);
+			seed = props.getProperty("seed");
+			secret = props.getProperty("secret");
+			KeyObjFactory.setRandomSeed(Base64.decodeBase64(seed));
+			KeyObjFactory.setSecret(Base64.decodeBase64(secret));
+			KeyObjFactory.genIV();
+			int ivsize = KeyObjFactory.getIV(ivbytes, seedsize);
+			props.setProperty("iv", Base64.encodeBase64(ivbytes).toString());
+	  		try
+			{
+				FileOutputStream fostream = new FileOutputStream(keyfile);
+				props.store(fostream, "");
+			}catch (Exception e)
+	  		{
+				  e.printStackTrace();
+				  System.exit(1);
+			}
+		}
+	  }catch(Exception e)
+	  {
+		  e.printStackTrace();
+		  System.exit(1);
+	  }
 
-	  //File propsFile = new File();
-	  
-	  //FileInputStream in = new FileInputStream();
-	  
-	  
-
-	  log.info("initializing");
+	log.info("Initialized KeyObjFactory");
   }
   
   @RequestMapping(value="/key/{clientid}/{firmid}")
   public @ResponseBody KeyObj getKey(@PathVariable String clientid,@PathVariable String firmid){
-	  //TODO: Horribly insecure and hacky. Need to check proper limit
-          //of input params and set size of cliarr/firmarr based on that size
-          // Also fix keygen methods.
 	  int len = Base64.decodeBase64(clientid).length;
 	  int len2 = Base64.decodeBase64(firmid).length;
           byte cliarr[] = new byte[len];
